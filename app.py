855eb4b9736bdfc7412c401fbafd938db5de2a26
oweniannucci-create <oweniannucci@icloud.com>
2026-05-06 18:30:33 -0400

    zxc

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ FORMAT:
 
         try:
             response = client.chat.completions.create(
-                model="mixtral-8x7b-32768",
+                model="llama-3.3-70b-versatile",
                 messages=[
                     {
                         "role": "system",
